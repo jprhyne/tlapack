43daa0753ffac04da347257e65a9d404aee6c9ba
Johnathan Rhyne <rhynejohnathan@gmail.com>
2023-02-18 09:45:39 -0700

    reverting stashes

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ set -x                  # Echo the commands
 cmake -B build -GNinja  # Generate the files with Ninja. 
 #To enable debugging symbols replace above line with cmake -DCMAKE_BUILD_TYPE=DEBUG --build build
 cmake --build build     # Actually compile
-/home/langou/Repositories/tlapack.git/build/test/test_eispack_hqr
+#/home/langou/Repositories/tlapack.git/build/test/test_eispack_hqr
